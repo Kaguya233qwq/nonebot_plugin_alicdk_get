9e041a42557733357feaf3d7e62ef376e3f55a03
zilong <1435608435@qq.com>
2022-09-21 11:48:36 +0800

    更新文档

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        "Natural Language :: Chinese (Simplified)"
     ],
     include_package_data=True,
     platforms="any",
