b4311089fad235128baee888360adeee659655c1
zilong <1435608435@qq.com>
2022-09-29 16:02:59 +0800

    更新pip包1.1.0版本

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
     long_description = f.read()
 setuptools.setup(
     name='nonebot-plugin-alicdk-get',
-    version='1.0.1',
+    version='1.1.0',
     author='Kaguya233qwq',
     author_email='1435608435@qq.com',
     keywords=["pip", "nonebot2", "nonebot", "alidrive", "nonebot_plugin"],
